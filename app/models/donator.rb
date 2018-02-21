31162f087aab639a10f42ef3285a6d69b7f8ab97
Mijail Rondon <mijail@gnu.org.ve>
2018-02-21 19:03:55 -0400

    Hide doctype

--- app/models/donator.rb
+++ app/models/donator.rb
@@ -4,12 +4,13 @@ class Donator < ApplicationRecord
   has_attached_file :doc_image, default_url: nil
   validates_attachment_content_type :doc_image, :rut_image, content_type: %w(image/jpeg image/jpg image/png application/pdf)
   validates :document, format: { with: /\A\d{7,10}\z/ }
-  validates_presence_of :firstname, :lastname, :doctype, :document, :amount, :rut_image, :doc_image, :email, :mobilephone, :address
+  validates_presence_of :firstname, :lastname, :document, :amount, :rut_image, :doc_image, :email, :mobilephone, :address
   validates_numericality_of :amount, greater_than_or_equal_to: 50_000, less_than_or_equal_to: 5_000_000_000
   validates_with AttachmentSizeValidator, attributes: :doc_image, less_than: 1.megabytes
   validates_with AttachmentSizeValidator, attributes: :rut_image, less_than: 1.megabytes
   private
   def add_slug
     self.slug = SecureRandom.uuid
+    self.doctype = 'CC'
   end
 end
