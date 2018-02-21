e2a8d22b16d85738992fec9e2296f9e0bf616985
Mijail Rondon <mijail@gnu.org.ve>
2018-02-21 18:19:20 -0400

    Add PDF and document format

--- app/models/donator.rb
+++ app/models/donator.rb
@@ -1,8 +1,9 @@
 class Donator < ApplicationRecord
   before_create :add_slug
-  has_attached_file :rut_image, styles: { medium: "800x800>" }, default_url: nil
-  has_attached_file :doc_image, styles: { medium: "800x800>" }, default_url: nil
-  validates_attachment_content_type :doc_image, :rut_image, content_type: /\Aimage\/.*\z/
+  has_attached_file :rut_image, default_url: nil
+  has_attached_file :doc_image, default_url: nil
+  validates_attachment_content_type :doc_image, :rut_image, content_type: %w(image/jpeg image/jpg image/png application/pdf)
+  validates :document, format: { with: /\A\d{7,10}\z/ }
   validates_presence_of :firstname, :lastname, :doctype, :document, :amount, :rut_image, :doc_image, :email, :mobilephone, :address
   validates_numericality_of :amount, greater_than_or_equal_to: 50_000, less_than_or_equal_to: 5_000_000_000
   validates_with AttachmentSizeValidator, attributes: :doc_image, less_than: 1.megabytes
