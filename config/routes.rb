4f8c1f6524f4cd440a6d52ce06287fbb1be4a6b7
Mijail Rondon <mijail@gnu.org.ve>
2018-02-28 01:50:31 -0400

    Validando path

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'donators#new', as: :donators
   post '/', to: 'donators#create'
+  get '/validando', to: 'donators#validating', as: :validating
   get '/:slug', to: 'donators#show', as: :donator
   get '/:slug/pagado', to: 'donators#pagado', as: :donator_pagado
   post '/:slug', to: 'donators#pagado_callback', as: :pagado_callback
-  get '/validando', to: 'donators#validating', as: :validating
+
 
   get '/responses', to: 'donations#responses', as: :responses
   post '/responses', to: 'donations#save_response', as: :save_response
