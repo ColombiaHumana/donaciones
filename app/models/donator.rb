157056ac6f0b20f75b2f3dd1d0f92aba7a56e844
Mijail Rondon <mijail@gnu.org.ve>
2018-03-01 03:18:10 -0400

    Validate donation minimum 5000

--- app/models/donator.rb
+++ app/models/donator.rb
@@ -5,7 +5,7 @@ class Donator < ApplicationRecord
   validates_attachment_content_type :doc_image, :rut_image, content_type: %w(image/jpeg image/jpg image/png application/pdf)
   validates :document, format: { with: /\A\d{7,10}\z/ }
   validates_presence_of :firstname, :lastname, :document, :amount, :rut_image, :doc_image, :email, :mobilephone, :address, :job
-  validates_numericality_of :amount, greater_than_or_equal_to: 50_000, less_than_or_equal_to: 5_000_000_000
+  validates_numericality_of :amount, greater_than_or_equal_to: 5_000, less_than_or_equal_to: 5_000_000_000
   validates_with AttachmentSizeValidator, attributes: :doc_image, less_than: 5.megabytes
   validates_with AttachmentSizeValidator, attributes: :rut_image, less_than: 5.megabytes
   private
