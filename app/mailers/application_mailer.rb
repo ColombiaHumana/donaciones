ac8e4617b488850c5c3c838c8d7438a33fd1b03d
Mijail Rondon <mijail.rondon@gmail.com>
2018-04-14 17:21:21 -0500

    From name in emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'donaciones@petro.com.co'
+  default from: 'Donaciones Colombia Humana <donaciones@petro.com.co>'
   layout 'mailer'
 end
